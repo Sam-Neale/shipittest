b41463cff61c8d40fc19dc396a0c23301f330fd9
Sam Neale <samneale@icloud.com>
2022-11-09 20:25:00 +1100

    init

--- shipitfile.js
+++ shipitfile.js
@@ -1,10 +1,12 @@
+const path = require("path");
+
 module.exports = function (shipit) {
     require('shipit-local')(shipit);
 
     shipit.initConfig({
         default: {
-            deployTo: '/var/www',
-            repositoryUrl: 'https://github.com/samneale/my-project.git'
+            deployTo: path.join(__dirname, '/../demo'),
+            repositoryUrl: 'https://github.com/Sam-Neale/shipittest.git'
         }
     });
 };
